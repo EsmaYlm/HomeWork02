004250ef08add22247b4870bf88f545122ca60ad
EsmaYlm <esmaylm5353@gmail.com>
2024-05-05 13:43:57 +0300

    HomeWork02

---
+++ src/Asalsayi.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Asalsayi {
+    public static void main(String[] args) {
+        int sayi = 17;
+        Asalsayi AsalSayi;
+        if (Asalsayi.asalMi(sayi)) {
+            System.out.println(sayi + " asal bir sayıdır.");
+        } else {
+            System.out.println(sayi + " asal bir sayı değildir.");
+        }
+    }
+
+    public static boolean asalMi(int sayi) {
+        if (sayi <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(sayi); i++) {
+            if (sayi % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
